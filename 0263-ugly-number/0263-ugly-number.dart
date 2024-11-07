ce4319bcbfb8503d4a18d7168803abe2865185c6
Siddhes Das <105408894+CoderSid007@users.noreply.github.com>
2024-11-07 11:46:01 +0530

    Time: 0 ms (100%), Space: 148.4 MB (58.33%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.dart
@@ -0,0 +1,22 @@
+class Solution {
+  bool isUgly(int n) {
+    if(n<=0){
+        return false;
+    }else{
+        while(n>=0){
+            if(n==1){
+                return true;
+            }else if(n%2==0){
+                n=n~/2;
+            }else if(n%3==0){
+                n=n~/3;
+            }else if(n%5==0){
+                n=n~/5;
+            }else{
+                return false;
+            }
+        }
+    }
+    return true;
+  }
+}
