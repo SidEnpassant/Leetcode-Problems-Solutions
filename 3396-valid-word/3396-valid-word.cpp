515287e7e4c0116f7b46aa708759fb306c9f5e64
Siddhes Das <105408894+SidEnpassant@users.noreply.github.com>
2025-07-16 01:28:17 +0530

    Time: 0 ms (100%), Space: 7.9 MB (86.98%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if(word.length() < 3) return false;
+
+        bool acheKiVowel = false;
+        bool acheKiConstant = false;
+
+        for(char charac : word){
+            if(!isalnum(charac)) return false;
+
+
+            if(isalpha(charac)){
+
+                char lowecase = tolower(charac);
+                if(lowecase == 'a' || lowecase == 'e' || lowecase == 'i' || lowecase == 'o' || lowecase == 'u') {
+                    acheKiVowel = true;
+                }
+                else
+                {
+                    acheKiConstant = true;
+                }
+
+            }
+            
+            if(acheKiVowel && acheKiConstant) continue;
+        }
+        return(acheKiVowel && acheKiConstant);
+    }
+};
