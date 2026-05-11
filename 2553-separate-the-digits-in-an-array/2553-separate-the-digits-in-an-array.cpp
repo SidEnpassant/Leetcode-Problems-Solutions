5c3205ee533a8554a527769b4ff329c469be05d2
Siddhes Das <105408894+SidEnpassant@users.noreply.github.com>
2026-05-11 14:57:35 +0530

    Time: 7 ms (35.43%), Space: 16.5 MB (7.95%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -6,8 +6,8 @@ public:
         for(auto num : nums){
             string str = to_string(num);
             for(auto stri : str){
-                string s(1 , stri);//creating a string with one occurence of stri
-                //int res = stri - '0'; // converting the character digit to an integer as characters are stored using ASCII values
+                string s(1 , stri);//creating a string with one occurence of stri  // THIS APPROACH TAKES MORE TIME
+                //int res = stri - '0'; // converting the character digit to an integer as characters are stored using ASCII values // THIS APPROACH TAKES LESS TIME
                 int res = stoi(s);
                 result.push_back(res);
             }
